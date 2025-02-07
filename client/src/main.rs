101169a7c951804c9b306d58462ac1577c6c7e3b
347Online | Katie Janzen <katiejanzen@347online.me>
2025-02-06 20:05:14 -0600

    Tinkering

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,8 +1,32 @@
-use silence::udp::client::Client;
+use std::net::{Ipv4Addr, Ipv6Addr};
+
+use common::DEFAULT_PORT;
+use silence::{
+    packet::VoipPacket,
+    udp::client::{Client, establish_connection},
+};
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() {
-    // let client = Client::new(uuid, remote_addr);
-
     println!("Hello from the client!");
+
+    let uuid = Uuid::now_v7();
+    let port = DEFAULT_PORT;
+    let addr = (Ipv6Addr::LOCALHOST, 12412);
+    let client = Client::new(uuid, addr).await.unwrap();
+    match establish_connection(addr).await {
+        Ok(sock) => {
+            sock.connect(addr).await.unwrap();
+            dbg!(sock);
+        }
+        Err(err) => eprintln!("ERROR: {err}"),
+    };
+    match client
+        .send_message(&VoipPacket(b"Hello world".into()))
+        .await
+    {
+        Ok(x) => println!("Sent, number was {x}"),
+        Err(err) => eprintln!("ERROR: {err}"),
+    }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,25 @@
 use common::DEFAULT_PORT;
-use silence::udp::server::Server;
+use silence::{
+    packet::{VoipHeader, VoipMessageType, VoipPacket},
+    udp::server::Server,
+};
 
 #[tokio::main]
 async fn main() {
     let port = DEFAULT_PORT;
 
-    let server = Server::new(port);
+    let mut server = Server::new(port).await.unwrap();
 
-    println!("Hello from the server!");
+    println!("Listening on port {}", port);
+
+    loop {
+        if let Some((header, bytes, sock)) = server.message_receiver().recv().await {
+            dbg!(header, bytes, sock);
+        }
+
+        server
+            .reply_to_clients(VoipPacket(Vec::from(b"Hello world")))
+            .await
+            .unwrap();
+    }
 }
